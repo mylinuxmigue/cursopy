19b311c04f0be4962c5fd6a092b6d38d3d49d58e
mylinuxmigue <loremigue1213@gmail.com>
2021-11-17 23:22:23 -0600

    vimos if  day 2

--- day2/e2.py
+++ day2/e2.py
@@ -3,14 +3,24 @@ year = int(input("Type a year: "))
 
 is_leap = False
 
-if  year % 4 == 0:
+"""if  year % 4 == 0:
     if year % 100 ==0:
         if year % 400 ==0:
             is_leap = True
     elif year % 100 != 0:
-        is_leap = True
+        is_leap = True"""
+
+#otra forma de hacer el programa dos 
+if year % 4 == 0 and year %100 == 0 and year % 400==0:
+    is_leap =True
+elif year % 4 == 0 and year %100 != 0:
+    is_leap = True
+
 
 if is_leap:
     print(f"{year} is leap")
-elif is_leap  == False:
+elif not is_leap:  #esta comparando un valor falso
     print(f"{year} is not leap")
+
+#elif is_leap  == False:
+    #print(f"{year} is not leap")
---
+++ day2/e3.py
@@ -0,0 +1,32 @@
+from re import T
+
+
+heigth = int(input("What is height in cm?"))
+
+can_ride = False
+
+if heigth >= 120:
+    can_ride = True
+    #age < 12 --- $5
+    #age <= 18 --- $7
+    #age <= 45 --- $9
+    #free
+
+if can_ride:
+    age = int(input("How old are you?: "))
+    ticket = 0
+    if age < 12:
+        #ticket = ticket + 5
+        ticket += 5
+    elif age <= 18: 
+        ticket +=7
+    elif age <= 45:
+        ticket += 9
+    photo = input("Do you want a photo?: (y/n)").lower()
+    if photo == "y":
+        ticket +=3
+    print(f"you have to pay {ticket}")
+elif not can_ride:
+    print("You cannot ride")
+
+ 
