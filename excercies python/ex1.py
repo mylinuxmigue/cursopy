919a0a5f6ec6892414142e30f35245397927c244
mylinuxmigue <loremigue1213@gmail.com>
2021-11-20 21:15:50 -0600

    ecercicio de tare

---
+++ excercies python/ex1.py
@@ -0,0 +1,12 @@
+print("Welcome to treasure island your mission is to find the trasure.")
+
+game = input("left or rigth? (l/r)").lower()
+
+if game == "l":
+    if input("swim or wait? (s/w)").lower() == "w":
+    else:
+        print("Attacked by trout/ GAMA OVER")
+        if input("which door, red or blue? (r/b)").lower() == 
+else:
+    print("Fall into a hole / GAME OVER")
+    
