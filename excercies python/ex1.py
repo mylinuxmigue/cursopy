3edb430d68423bf9ed31771d76df944fbd23dcc4
mylinuxmigue <loremigue1213@gmail.com>
2021-11-21 22:02:21 -0600

    ejercicio de tarea

--- excercies python/ex1.py
+++ excercies python/ex1.py
@@ -3,10 +3,20 @@ print("Welcome to treasure island your mission is to find the trasure.")
 game = input("left or rigth? (l/r)").lower()
 
 if game == "l":
-    if input("swim or wait? (s/w)").lower() == "w":
-    else:
-        print("Attacked by trout/ GAMA OVER")
-        if input("which door, red or blue? (r/b)").lower() == 
+    game = input("swim or wait? (s/w)").lower()
+    if  game == "w":
+        game = (input("whitch door? (blue, red, yellow)")).lower()
+        if game == "blue":
+            print("Eaten by beasts.")
+        elif game == "yellow":
+            print("you win")
+        elif game == "red":
+            print("Burned by fire")
+        else:
+            print("GAME OVER")
+    else: 
+        print("Attacked by trout")
 else:
-    print("Fall into a hole / GAME OVER")
-    
+    print("Fall into a hole") 
+
+
