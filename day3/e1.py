7f1d7904ddf62cf9ab6dacbf3b8b08dcb0821f73
mylinuxmigue <loremigue1213@gmail.com>
2021-11-22 20:07:44 -0600

    ejercicio de tarea2

--- day3/e1.py
+++ day3/e1.py
@@ -3,7 +3,7 @@ import random
 list1 = [12,13,16,1.6,True,"hello",12,7]
 #print(random.choice(list1))
 #print(random.randint(1,100))
-print(random.random())
+#print(random.random())
 
 
 var = round(random.uniform(1.2,2.4),4)
---
+++ excercies python/ex2.py
@@ -0,0 +1,7 @@
+import random 
+
+print("welcome to the restaurant, add ypur names to randomly choose who will pay? ")
+
+print("el sigueinte numero en pagar es: ")
+ 
+print(random.randint(1,4))
