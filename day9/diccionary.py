bfc08704ab3b8335c6f1180937b4883f9489a75c
mylinuxmigue <loremiguel1213@gmail.com>
2021-12-19 23:46:57 -0600

    -

--- day9/diccionary.py
+++ day9/diccionary.py
@@ -5,7 +5,7 @@ dictionary['flash'] = 'he is the fastest man over the world'
 
 dictionary.pop('superman') 
 '''
-dictionary = {"superman":{'power1':'he can fly','power 2':'he is strong'},"flash":'he is the fastest', 
+dictionary = {"superman":{'power1':'he can fly','power 2':'strong'},"flash":'he is the fastest', 
 'green lantern':'he is the choisen one'}
 
 for key in dictionary:
