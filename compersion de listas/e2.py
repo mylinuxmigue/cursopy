36e0c8f3b97ff4f59d59f31e7f73ec7a6503ec3c
mylinuxmigue <loremiguel1213@gmail.com>
2022-02-21 11:36:05 -0600

    listas compresion

--- compersion de listas/e2.py
+++ compersion de listas/e2.py
@@ -1,21 +1,22 @@
-from typing import final
+
 
 
 def vowel_removed(str):
     vowels = ['a','e','i','o','u']
 
     #first way 
-    '''final_list = []
+    final_list = []
     for letter in str:
         if letter not in vowels:
             final_list.append(letter)
-'''
+    return final_list
+
 
     #second way 
     #final_list = [letter for letter in str if letter not in vowels]
     #text = ''.join(final_list)
 
-    return ''.join([letter for letter in str if letter not in vowels])
+   # return ''.join([letter for letter in str if letter not in vowels])
 
-word = vowel_removed('mellamomiguel')
+word = vowel_removed('miguela')
 print(word)
